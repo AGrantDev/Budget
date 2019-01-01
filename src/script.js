dbfe6a46a2f6b22bfad32d608c79321f9a1c50d7
Anthony Grant <Anthony.Grant.Mail@gmail.com>
2018-12-31 18:54:38 -0800

    adding JS

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
+let salaryArray = [];
+let companyArray = [];
+
+let salaryButton = document.getElementById("addSalary")
+
+function addSalary(){
+    let salaryString = document.getElementById("salary").value;
+    let salary = parseInt(salaryString,10);
+    salaryArray.push(salary);
+}
+
+function addCompany(){
+    let company = document.getElementById("company").value;
+    companyArray.push(company);
+}
+
+salaryButton.addEventListener("click", addSalary);
+salaryButton.addEventListener("click", addCompany);
+
 let button = document.getElementsByClassName("window-collapse-button");
 let windowBody = document.getElementsByClassName("window-body");
 
